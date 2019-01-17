ff50fc0a981e0c10d62385818525d8d39eb47059
A B B A S <plus.irani@gmail.com>
2019-01-17 18:08:32 +0330

    upload

---
+++ precache-manifest.bf785f54ab6caf8828a7a2125c84dad5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9464550e5ebca97d9e3a6b204742e95",
+    "url": "/static/media/room_booking.b9464550.svg"
+  },
+  {
+    "revision": "c8cc256db25cf073a535",
+    "url": "/static/css/main.25588d98.chunk.css"
+  },
+  {
+    "revision": "4fe0dea37dd817e16a72",
+    "url": "/static/js/1.4fe0dea3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "463b4d4cdf31c0f8e48a8a915ce4993a",
+    "url": "/static/media/bg_graidents.463b4d4c.svg"
+  },
+  {
+    "revision": "139e0ce5bfb8435352d8cd29b53bd59f",
+    "url": "/static/media/check-mark.139e0ce5.svg"
+  },
+  {
+    "revision": "352f0412e48eecdd71cbf521604d558d",
+    "url": "/static/media/user-circle-o.352f0412.svg"
+  },
+  {
+    "revision": "4270b83ae15a41795fad278e4dbb7299",
+    "url": "/static/media/icon_logout.4270b83a.svg"
+  },
+  {
+    "revision": "2a2bf5496443289629a3cf54dc27d5e0",
+    "url": "/static/media/dash.2a2bf549.svg"
+  },
+  {
+    "revision": "c8cc256db25cf073a535",
+    "url": "/static/js/main.c8cc256d.chunk.js"
+  },
+  {
+    "revision": "5ebb778ee90864a7cfc6d7095840a6cb",
+    "url": "/static/media/events.5ebb778e.svg"
+  },
+  {
+    "revision": "229ee20b655465e8c20ba2fe8436d4cc",
+    "url": "/static/media/user.229ee20b.svg"
+  },
+  {
+    "revision": "c7fc55a87a65addaad05c1b65894d30e",
+    "url": "/static/media/settings.c7fc55a8.svg"
+  },
+  {
+    "revision": "157c4d8e1a788e87c738a35d2f29da0d",
+    "url": "/static/media/dash_home_bread_crumb.157c4d8e.svg"
+  },
+  {
+    "revision": "02b6fa5d4d760ce4788abd59b66bc77c",
+    "url": "/static/media/button_plus.02b6fa5d.svg"
+  },
+  {
+    "revision": "55830e1f384cd8e877b8f2a83209393a",
+    "url": "/static/media/search.55830e1f.svg"
+  },
+  {
+    "revision": "5c44d65021257eb6658c8394f682226a",
+    "url": "/static/media/add-image_icon.5c44d650.svg"
+  },
+  {
+    "revision": "218378a00f31cf34b8aaf5533729071c",
+    "url": "/index.html"
+  }
+];
